44f645ceac60d4715faa77c3b8f848d86ebcad54
Luiz Eduardo Alencar Pacheco <luizeduardopacheco@unifor.br>
2024-05-13 16:10:13 -0300

    Feature Login

--- app/src/main/java/com/example/obrasacessiveis/FazerCadastroActivity.kt
+++ app/src/main/java/com/example/obrasacessiveis/FazerCadastroActivity.kt
@@ -1,14 +1,26 @@
 package com.example.obrasacessiveis
 
-import android.annotation.SuppressLint
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
-import android.widget.ImageButton
 import android.widget.Toast
 import com.google.firebase.firestore.FirebaseFirestore
+import java.math.BigInteger
+import java.security.MessageDigest
+
+fun calcularHashMD5(senha: String): String {
+    val md = MessageDigest.getInstance("MD5")
+    val messageDigest = md.digest(senha.toByteArray())
+    val no = BigInteger(1, messageDigest)
+    var hashtext = no.toString(16)
+    while (hashtext.length < 32) {
+        hashtext = "0$hashtext"
+    }
+    return hashtext
+}
+
 
 class FazerCadastroActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,7 +36,9 @@ class FazerCadastroActivity : Activity() {
             if(senha.text.toString() != confirmarSenha.text.toString()) {
                 Toast.makeText(this, "As senhas não estão iguais", Toast.LENGTH_SHORT).show()
             } else {
-                Cadastrar(nome, email, senha, confirmarSenha)
+                val senhaCriptografada = calcularHashMD5(senha.text.toString())
+                val confirmarSenhaCriptografada = calcularHashMD5(confirmarSenha.text.toString())
+                Cadastrar(nome, email, senhaCriptografada, confirmarSenhaCriptografada)
                 TrocarParaLogin()
             }
         }
@@ -33,12 +47,12 @@ class FazerCadastroActivity : Activity() {
         val telaAdicionar = Intent(this, MainActivity::class.java)
         startActivity(telaAdicionar)
     }
-    private fun Cadastrar(nome: EditText, email: EditText, senha: EditText, confirmarSenha: EditText,) {
+    private fun Cadastrar(nome: EditText, email: EditText, senha: String, confirmarSenha: String,) {
         FirebaseFirestore.getInstance().collection("Cadastro").add(mapOf(
             "nome" to nome.text.toString(),
             "email" to email.text.toString(),
-            "senha" to senha.text.toString(),
-            "confirmarSenha" to confirmarSenha.text.toString()))
+            "senha" to senha,
+            "confirmarSenha" to confirmarSenha))
     }
 
 }
--- app/src/main/java/com/example/obrasacessiveis/MainActivity.kt
+++ app/src/main/java/com/example/obrasacessiveis/MainActivity.kt
@@ -3,25 +3,50 @@ package com.example.obrasacessiveis
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.firestore.ktx.toObject
 import com.google.firebase.ktx.Firebase
+import java.math.BigInteger
+import java.security.MessageDigest
+
+fun hashPassword(password: String): String {
+    val md = MessageDigest.getInstance("MD5")
+    val digest = md.digest(password.toByteArray())
+    return BigInteger(1, digest).toString(16).padStart(32, '0')
+}
 
 class MainActivity : Activity() {
+    private val db = Firebase.firestore
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val view = View(this)
         setContentView(R.layout.activity_login)
 
         val button = findViewById<Button>(R.id.loginButton)
         val souUsuario = findViewById<TextView>(R.id.souUsuario)
         val cadastrese = findViewById<TextView>(R.id.criarcadastro)
+        val emailEditText = findViewById<EditText>(R.id.emailEditText)
+        val passwordEditText = findViewById<EditText>(R.id.passwordEditText)
 
-        button.setOnClickListener() {
-            TrocarDeTela()
+        button.setOnClickListener {
+            val email = emailEditText.text.toString()
+            val password = passwordEditText.text.toString()
+            //Log.d("MainActivity", "EMAIL: $email")
+            //Log.d("NICOLE", "SENHA: $password")
+
+            if (email.isNotEmpty() && password.isNotEmpty()) {
+                login(email, password)
+            } else {
+                Toast.makeText(this, "Preencha todos os campos", Toast.LENGTH_SHORT).show()
+            }
         }
+
+
         cadastrese.setOnClickListener() {
             FazerCadastro()
         }
@@ -29,11 +54,30 @@ class MainActivity : Activity() {
             TrocarDeTela2()
         }
 
+
     }
-    private fun TrocarDeTela() {
-        val outraTela = Intent(this, SessionAdmin::class.java)
-        startActivity(outraTela)
+    private fun login(email: String, password: String) {
+        db.collection("Cadastro").whereEqualTo("email", email).get()
+            .addOnSuccessListener { documents ->
+                for (document in documents) {
+                    val savedPassword = document.getString("senha")
+                    val md5Password = hashPassword(password)
+                    if (savedPassword == md5Password) {
+                        val intent = Intent(this, SessionAdmin::class.java)
+                        startActivity(intent)
+                        Toast.makeText(this, "Bem-vindo ao ObrasApp", Toast.LENGTH_SHORT).show()
+                        return@addOnSuccessListener
+                    }
+                }
+                Toast.makeText(this, "Credenciais inválidas", Toast.LENGTH_SHORT).show()
+            }
+            .addOnFailureListener { exception ->
+                Toast.makeText(this, "Erro ao realizar login: ${exception.message}", Toast.LENGTH_SHORT).show()
+            }
     }
+
+
+
     private fun TrocarDeTela2() {
         val outraTela = Intent(this, Session::class.java)
         startActivity(outraTela)
